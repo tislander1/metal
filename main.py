68b7f0487c9d03c432ef35f472d3a031b88eec0b
Tislander1 <blurryprojection@gmail.com>
2024-10-26 09:41:22 -0600

    Add find retr_external contours

--- main.py
+++ main.py
@@ -11,5 +11,18 @@ im_bw_dilated = cv2.dilate(im_bw_eroded, kernel)
 cv2.imwrite('bw_image.png', im_bw)
 #cv2.imwrite('bw_image_eroded.png', im_bw_eroded)
 cv2.imwrite('bw_image_dilated.png', im_bw_dilated)
+
+contours, contours_hierarchy = cv2.findContours(im_bw_dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+x = 2
+
+# Draw the contours on the original image
+color_pic = cv2.cvtColor(im_gray, cv2.COLOR_GRAY2RGB)
+cv2.drawContours(color_pic, contours, -1, (0, 255, 0), 1)
+
+# Display the image with contours
+cv2.imshow('Contours', color_pic)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
 print('Threshold: '+str(threshold))
 
